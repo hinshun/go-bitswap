2e915dd7ff3520307d56f4baca0d0f1dbcc6e4b3
Edgar Lee <edgarl@netflix.com>
2019-08-26 18:38:41 -0700

    Update TaskWorkerCount to 16

--- workers.go
+++ workers.go
@@ -13,7 +13,7 @@ import (
 
 // TaskWorkerCount is the total number of simultaneous threads sending
 // outgoing messages
-var TaskWorkerCount = 8
+var TaskWorkerCount = 16
 
 func (bs *Bitswap) startWorkers(ctx context.Context, px process.Process) {
 
